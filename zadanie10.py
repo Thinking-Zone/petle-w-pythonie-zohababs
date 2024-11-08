12062d60d9a2b9a85008da58ecb7eefae469f6c2
zohababs <zofiiababs@gmail.com>
2024-11-08 13:46:20 +0100

    zadanie10.py

---
+++ zadanie10.py
@@ -0,0 +1,6 @@
+# lista postaci z lego ninjago (Lloyd sie nieliczy bo jest beznadziejy)
+postcie_z_lego_ninjago = ["Jay", "Zane", "Cole", "Kay", "Sensei Wu"]
+
+# wypisuje poacie
+for imie in postcie_z_lego_ninjago:
+    print(imie)
